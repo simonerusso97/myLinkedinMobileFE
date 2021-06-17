3db567d5a51033fcc1230c10f6726f94015572e2
Simone Russo <97.russo@gmail.com>
2021-06-17 17:36:00 +0200

    connessione al post per getPost

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -15,14 +15,9 @@ import {PostService} from '../../services/post.service';
 export class HomePage implements OnInit{
 
   user: Offeror | Applicant;
-  post: Post = {} as Post;
-  post2: Post = {} as Post;
-  post3: Post = {} as Post;
-
   postList: Post[] = [];
+  err = false;
 
-  company: Company = {} as Company;
-  structure: Structure = {} as Structure;
   constructor(private routes: Router, private postService: PostService) {
     this.user = JSON.parse(sessionStorage.getItem('user'));
     if(this.user == null){
@@ -31,56 +26,14 @@ export class HomePage implements OnInit{
   }
 
   ngOnInit(): void {
-    this.company = {
-      description: 'prova',
-      id: 0,
-      name: 'azienda prova',
-      pwd: '1234',
-      sector: 'IT'
-
-    };
-    this.structure = {
-      description: 'strutture prova',
-      id: 0,
-      name: 'struttura prova',
-      userType: 'offeror'
-
-    };
-    this.post = {
-      company: this.company,
-      createdBy: this.user,
-      hide: false,
-      id: 0,
-      name: 'Post di prova 1',
-      pubblicationDate: new Date('2020-06-15'),
-      report: 0,
-      structure: this.structure
-    };
-    this.post2 = {
-      company: this.company,
-      createdBy: this.user,
-      hide: false,
-      id: 0,
-      name: 'Post di prova 2',
-      pubblicationDate: new Date('2020-06-15'),
-      report: 0,
-      structure: this.structure
-    };
-    this.post3 = {
-      company: this.company,
-      createdBy: this.user,
-      hide: false,
-      id: 0,
-      name: 'Post di prova 3',
-      pubblicationDate: new Date('2020-06-15'),
-      report: 0,
-      structure: this.structure
-    };
-    this.postList.unshift(this.post);
-    this.postList.unshift(this.post2);
-    this.postList.unshift(this.post3);
-    this.postList.unshift(this.post);
-
+    this.postService.getPost(this.user).subscribe(
+      response => {
+        this.postList = response;
+        this.err = false;
+      },
+      error => {
+        this.err = true;
+      });
 
   }
 
