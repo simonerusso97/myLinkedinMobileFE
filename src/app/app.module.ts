32734d6407a731a469f62a5231065164f2ee4340
Andrea De Rinaldis <andrea.derinaldis1@studenti.unisalento.it>
2021-06-17 20:50:57 +0200

    geoloc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {SignupPageModule} from './pages/signup/signup.module';
 import {HttpClientModule} from '@angular/common/http';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
     imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SignupPageModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [Geolocation,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/regular-login/regular-login.component.ts
+++ src/app/components/regular-login/regular-login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Regular} from '../../models/regular';
 import {UserService} from '../../services/user.service';
 import {Router} from '@angular/router';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-regular-login',
@@ -14,11 +15,26 @@ export class RegularLoginComponent implements OnInit {
   bannedError = false;
   regular: Regular = {} as Regular;
 
-  constructor(private userService: UserService, private routes: Router) { }
-
+  constructor(private userService: UserService, private routes: Router, private geo: Geolocation) { }
+  lat:number=0;
+  long:number=0;
   ngOnInit() {}
 
   onSubmit() {
+    
+    this.geo.getCurrentPosition({
+      timeout:1000,
+      enableHighAccuracy:true
+    }).then((data)=>{
+        this.lat=data.coords.latitude;
+        this.long=data.coords.longitude;
+    
+      }).catch((e)=>{
+      console.log(e);
+    });
+    
+    
+    
     this.userService.login(this.regular).subscribe(
       response => {
         this.loginError = false;
