f8b93ef6baf2377d0805b871f1947be15cbd4991
Andrea De Rinaldis <andrea.derinaldis1@studenti.unisalento.it>
2021-06-17 13:40:18 +0200

    creato component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {SignupPageModule} from './pages/signup/signup.module';
 
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
---
+++ src/app/components/tool-bar-component/tool-bar-component.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-bar-component',
+  templateUrl: './tool-bar-component.component.html',
+  styleUrls: ['./tool-bar-component.component.scss'],
+})
+export class ToolBarComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
